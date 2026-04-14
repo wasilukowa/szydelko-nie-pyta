1acfa017f58026a609fb8cd0efdeee4be5f2a4d5
Natalia <wasiluk.natalia@gmail.com>
2026-04-14 14:53:23 +0200

    Initial commit - placeholder page with logo and social links

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <img src="/logo.svg" alt="Szydełko nie pyta" />
+      <div className="socials">
+        <a
+          href="https://www.instagram.com/szydelko_nie_pyta/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=61581252330329"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
